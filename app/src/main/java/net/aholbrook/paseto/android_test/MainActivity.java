f5eb401504b3e1bf73f68448597de72952b6d3c1
Andrew Holbrook <atholbro@gmail.com>
2019-08-08 17:06:22 -0400

    Fix example (remove apache commons).

--- app/src/main/java/net/aholbrook/paseto/android_test/MainActivity.java
+++ app/src/main/java/net/aholbrook/paseto/android_test/MainActivity.java
@@ -9,8 +9,6 @@ import net.aholbrook.paseto.service.LocalTokenService;
 import net.aholbrook.paseto.service.Token;
 import net.aholbrook.paseto.service.TokenService;
 
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
 import org.threeten.bp.Duration;
 import org.threeten.bp.Instant;
 
@@ -19,29 +17,37 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
 	private TextView text;
 
+	public static byte[] decode(String s) {
+		int len = s.length();
+		byte[] data = new byte[len/2];
+
+		for(int i = 0; i < len; i+=2){
+			data[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+		}
+
+		return data;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		text = findViewById(R.id.text);
-		try {
-			final byte[] key = Hex.decodeHex("707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f");
 
-			TokenService<Token> tokenService = PasetoBuilders.V2.localService(new LocalTokenService.KeyProvider() {
-				@Override
-				public byte[] getSecretKey() {
-					return key;
-				}
-			}, Token.class).build();
+		final byte[] key = decode("707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f");
 
-			Token token = new Token();
-			token.setExpiration(Instant.now().plus(Duration.ofDays(1)).getEpochSecond());
-			token.setTokenId(UUID.randomUUID().toString());
+		TokenService<Token> tokenService = PasetoBuilders.V2.localService(new LocalTokenService.KeyProvider() {
+			@Override
+			public byte[] getSecretKey() {
+				return key;
+			}
+		}, Token.class).build();
 
-			text.setText(tokenService.encode(token));
-		} catch (DecoderException e) {
-			e.printStackTrace();
-		}
+		Token token = new Token();
+		token.setExpiration(Instant.now().plus(Duration.ofDays(1)).getEpochSecond());
+		token.setTokenId(UUID.randomUUID().toString());
+
+		text.setText(tokenService.encode(token));
 	}
 }
